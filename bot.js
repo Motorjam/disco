3074470d968b3cabcf7a75babf79a454c7ac481d
Motorjam <53126725+Motorjam@users.noreply.github.com>
2019-07-20 18:05:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,6 +34,7 @@ client.on("message", async message => {
     if (args != ":black_circle:"||":page_facing_up:"||":scissors:") {
       message.reply("Say l!rps followed by either :black_circle: :page_facing_up: or :scissors: to play);
     }
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
