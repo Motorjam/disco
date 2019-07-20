dd4f868c7efb847c4fdc799769b49ce3fb91005a
Motorjam <53126725+Motorjam@users.noreply.github.com>
2019-07-20 18:07:00 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ client.on("message", async message => {
     message.reply("You rolled " + x);
   }
   if (command === "rps") {
-    if (args != ":black_circle:"||":page_facing_up:"||":scissors:") {
+    if (args !== ":black_circle:"||":page_facing_up:"||":scissors:") {
       message.reply("Say l!rps followed by either :black_circle: :page_facing_up: or :scissors: to play);
     }
   }
