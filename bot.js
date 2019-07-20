2d0254a19ed1290a9833a66ecc0146b6a163aeb2
Motorjam <53126725+Motorjam@users.noreply.github.com>
2019-07-20 17:42:43 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,13 +13,37 @@ client.on("message", async message => {
   const args = message.content.slice(2).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
+  function rng(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+    
   //commands start here
   if (command === "help") {
     message.reply("fuck you");
   }
   if (command === "plug") {
     message.reply("Follow me on Twitch, sub to me on YouTube :) https://www.twitch.tv/motorjam https://www.youtube.com/channel/UC93SMdJJOHf8tcF-PJ_ArxA");
-  }  
+  }
+  if (command === "rps") {
+    if (args != ":black_circle:"||":page_facing_up:"||":scissors:") {
+      message.reply("Say l!rps followed by either :black_circle: :page_facing_up: or :scissors: to play)
+    }else {
+      var msg1 = Array(3);
+	  msg1[1] = ":black_circle:";
+	  msg1[2] = ":page_facing_up:";
+	  msg1[3] = ":scissors:";
+      var throw = rng(1, 3);
+      if (throw == 1 && args == ":black_circle:" || throw == 2 && args == ":page_facing_up:" || throw == 3 && args == ":scissors:") {
+        message.channel.send(msg1[throw] + " It's a tie!"
+      }else if (throw == 2 && args == ":black_circle:" || throw == 3 && args == ":page_facing_up:" || throw == 1 && args == ":scissors:") {
+        message.channel.send(msg1[throw] + " I win!"
+      }else if (throw == 3 && args == ":black_circle:" || throw == 1 && args == ":page_facing_up:" || throw == 2 && args == ":scissors:") {
+        message.channel.send(msg1[throw] + " You win!"
+       }
+     }
+   }
 });
 
 client.login(process.env.BOT_TOKEN);
