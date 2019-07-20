cbeb17e4c9a1546c3bcbdb3a1edcfa5adc77bf51
Motorjam <53126725+Motorjam@users.noreply.github.com>
2019-07-20 17:11:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js'); 
 const client = new Discord.Client();
-const config = require("./config.json");
 
 client.on('ready', () => {   
     console.log('The bot is ready'); 
@@ -9,7 +8,7 @@ client.on('ready', () => {
 client.on("message", async message => {
   
   if(message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
+  if(message.content.indexOf("l!") !== 0) return;
   
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
@@ -17,9 +16,10 @@ client.on("message", async message => {
   //commands start here
   if (command === "help") {
     message.reply("fuck you");
-}
+  }
   if (command === "plug") {
     msg.reply("Follow me on Twitch, sub to me on YouTube :) https://www.twitch.tv/motorjam https://www.youtube.com/channel/UC93SMdJJOHf8tcF-PJ_ArxA");
+  }  
 });
 
 client.login(process.env.BOT_TOKEN);
