5d4b09f222ae210c61384c159689324dd2574b90
Motorjam <53126725+Motorjam@users.noreply.github.com>
2019-07-20 18:03:18 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,18 +33,7 @@ client.on("message", async message => {
   if (command === "rps") {
     if (args != ":black_circle:"||":page_facing_up:"||":scissors:") {
       message.reply("Say l!rps followed by either :black_circle: :page_facing_up: or :scissors: to play);
-    }else {
-      var msg1 = [":black_circle:", ":page_facing_up:", ":scissors:"];
-      var throw = rng(1, 3);
-      if (throw == 1 && args == ":black_circle:" || throw == 2 && args == ":page_facing_up:" || throw == 3 && args == ":scissors:") {
-        message.channel.send(msg1[throw] + " It's a tie!");
-      }else if (throw == 2 && args == ":black_circle:" || throw == 3 && args == ":page_facing_up:" || throw == 1 && args == ":scissors:") {
-        message.channel.send(msg1[throw] + " I win!");
-      }else if (throw == 3 && args == ":black_circle:" || throw == 1 && args == ":page_facing_up:" || throw == 2 && args == ":scissors:") {
-        message.channel.send(msg1[throw - 1] + " You win!");
-       }
-     }
-   }
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
