c7b34ce64b6f1c9d278ec3666112bacd70fb277a
Motorjam <53126725+Motorjam@users.noreply.github.com>
2019-07-20 19:15:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,9 +46,10 @@ client.on("message", async message => {
   }
   if (command === "rps") {
     const player = args.join(" ");
-    if (player !== "rock" || player !== "paper" || player !== "scissors") {
+    if (player === ":black_circle:" || player === ":page_facing_up:" || player === ":scissors:") {
+      message.channel.send("Nice.");
+    }else {
       message.reply("Say l!rps followed by either :black_circle: :page_facing_up: or :scissors: to play");
-    }
   }
 });
 
